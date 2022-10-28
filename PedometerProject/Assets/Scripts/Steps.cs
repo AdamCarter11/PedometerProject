11e9365f00ee9adab971d25133267dbc84b9a375
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-10-27 19:29:55 -0700

    Its working!

--- PedometerProject/Assets/Scripts/Steps.cs
+++ PedometerProject/Assets/Scripts/Steps.cs
@@ -8,6 +8,7 @@ using UnityEngine.Android;
 
 public class Steps : MonoBehaviour
 {
+    
     [SerializeField] TextMeshProUGUI stepDisplayText, workingDisplay;
     // Start is called before the first frame update
     void Start()
@@ -16,17 +17,20 @@ public class Steps : MonoBehaviour
         if (!Permission.HasUserAuthorizedPermission("android.permission.ACTIVITY_RECOGNITION"))
         {
             Permission.RequestUserPermission("android.permission.ACTIVITY_RECOGNITION");
+            //
         }
         
         InputSystem.EnableDevice(StepCounter.current);
         if(StepCounter.current.enabled){
             workingDisplay.text = "Working: True";
         }
-        
+
+
+        //times per second it is updated
+        //StepCounter.current.samplingFrequency = 5;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(StepCounter.current.enabled)
         {
