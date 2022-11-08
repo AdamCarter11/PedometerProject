8463a19fd575ceb01c837ab13e4f8cee2672479d
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-11-07 18:38:28 -0800

    More bug fixes

--- PedometerProject/Assets/Scripts/Steps.cs
+++ PedometerProject/Assets/Scripts/Steps.cs
@@ -10,15 +10,16 @@ using UnityEngine.Android;
 public class Steps : MonoBehaviour
 {
     
-    [SerializeField] TextMeshProUGUI stepDisplayText, workingDisplay;
+    [SerializeField] TextMeshProUGUI stepDisplayText, workingDisplay, dateDisplay, versionText;
     float lastStepVal;
     DateTime currTime;
     DateTime startTime; 
     int lastSteps = 0;
+    [HideInInspector] public static int totalSteps;
     // Start is called before the first frame update
     void Start()
     {
-        
+        print(PlayerPrefs.GetString("timeSaved"));
         if (!Permission.HasUserAuthorizedPermission("android.permission.ACTIVITY_RECOGNITION"))
         {
             Permission.RequestUserPermission("android.permission.ACTIVITY_RECOGNITION");
@@ -28,39 +29,53 @@ public class Steps : MonoBehaviour
         InputSystem.EnableDevice(StepCounter.current);
         if(StepCounter.current.enabled){
             workingDisplay.text = "Working: True";
+            if(!PlayerPrefs.HasKey("lastSteps")){
+                PlayerPrefs.SetInt("lastSteps", 0);
+            }
         }
-
+        
         if(!PlayerPrefs.HasKey("timeSaved")){
             currTime = DateTime.Now;
             //print(currTime);
             PlayerPrefs.SetString("timeSaved", currTime.ToString());
 
             //used for reseting steps at beginning of each day
+            /*
             if(StepCounter.current.enabled){
                 lastStepVal = StepCounter.current.stepCounter.ReadValue();
-                PlayerPrefs.SetInt("lastStepVal", lastSteps);
+                PlayerPrefs.SetInt("lastSteps", (int)lastStepVal);
             }
+            */
         }
     }
 
     private void Update() {
-
+        
         if(StepCounter.current.enabled)
         {
             int tempSteps = PlayerPrefs.GetInt("lastSteps");
-            stepDisplayText.text = ($"Steps: {StepCounter.current.stepCounter.ReadValue() - tempSteps}");
+            if(tempSteps <= 0){
+                tempSteps = 0;
+                PlayerPrefs.SetInt("lastSteps", 0);
+            }
+            
+            totalSteps = StepCounter.current.stepCounter.ReadValue() - tempSteps;
+            stepDisplayText.text = ($"Steps: {totalSteps}");
         }
 
         string originTime = PlayerPrefs.GetString("timeSaved");
-        if(DateTime.Now.ToString()[3] != originTime[3]){
+        int lastStepVar = PlayerPrefs.GetInt("lastSteps");
+        dateDisplay.text = "origin time: " + originTime + " last steps: " + lastStepVar;
+        versionText.text = DateTime.Now.ToString();
+        if(DateTime.Now.ToString()[3] != originTime[3] || DateTime.Now.ToString()[4] != originTime[4]){
+            //reset steps
+            lastSteps = StepCounter.current.stepCounter.ReadValue();
+            PlayerPrefs.SetInt("lastSteps", lastSteps);
+
             //new day (and save time)
             currTime = DateTime.Now;
             //print(currTime);
             PlayerPrefs.SetString("timeSaved", currTime.ToString());
-
-            //reset steps
-            lastSteps = StepCounter.current.stepCounter.ReadValue();
-            PlayerPrefs.SetInt("lastSteps", lastSteps);
         }
     }
 }
